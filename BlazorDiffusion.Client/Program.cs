10c1d588be25c071560e6f10fe59d2746d942802
Darren Reid <darren@reidmail.org>
2024-01-09 16:23:00 +1100

    Update blazor config.

--- BlazorDiffusion.Client/Program.cs
+++ BlazorDiffusion.Client/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorDiffusion.Client;
 using BlazorDiffusion.Client.UI;
+using BlazorDiffusion.ServiceModel;
 using Ljbc1994.Blazor.IntersectionObserver;
+using ServiceStack;
 using ServiceStack.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -21,4 +23,23 @@ builder.Services.AddScoped<KeyboardNavigation>();
 builder.Services.AddScoped<UserState>();
 builder.Services.AddIntersectionObserver();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+BlazorConfig.Set(new BlazorConfig
+{
+    IsWasm = true,
+    Services = app.Services,
+    ApiBaseUrl = apiBaseUrl,
+    AssetsBasePath = "https://cdn.diffusion.works",
+    FallbackAssetsBasePath = "https://pub-97bba6b94a944260b10a6e7d4bf98053.r2.dev",
+    EnableLogging = true,
+    EnableVerboseLogging = builder.HostEnvironment.IsDevelopment(),
+    DefaultProfileUrl = Icons.AnonUserUri,
+    OnApiErrorAsync = (request, apiError) =>
+    {
+        BlazorConfig.Instance.GetLog()?.LogDebug("\n\nOnApiErrorAsync(): {0}", apiError.Error.GetDetailedError());
+        return Task.CompletedTask;
+    }
+});
+
+await app.RunAsync();
